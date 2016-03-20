cbce16810e6d1a0b970f09da02bded5e11242b63
p3r7 <contact.perf@gmail.com>
2016-03-20 15:41:21 +0100

    cont.

--- provision.sh
+++ provision.sh
@@ -5,6 +5,19 @@ export DEBIAN_FRONTEND=noninteractive
 sudo aptitude update -q
 
 # -------------------------------------------------------------------------
+
+# standard user configuration
+
+mkdir $HOME/bin
+
+sudo cat >> $HOME/.bashrc <<EOF
+
+# Adding custom user bin folder
+PATH="\$HOME/bin:$PATH"
+EOF
+
+
+# -------------------------------------------------------------------------
 # SDL
 sudo aptitude install -q -y -f libsdl2-dev libsdl2-image-dev libsdl2-ttf-dev libsdl2-gfx-dev
 
@@ -24,8 +37,11 @@ cd /tmp/
 wget http://unhaut.fav.cc/psxsdk/psxsdk-20150729-linux-i686-toolchain.tar.xz
 tar xf psxsdk-20150729-linux-i686-toolchain.tar.xz
 sudo mv usr/local/psxsdk /usr/local/psxsdk
-sudo cat >> /home/vagrant/.bashrc <<EOF
+sudo cat >> $HOME/.bashrc <<EOF
 
 # Adding PSXSDK to PATH
 PATH="/usr/local/psxsdk/bin:\$PATH"
 EOF
+
+ln -sf /vagrant/psxsdk-make.sh $HOME/bin/psxsdk-make
+chmod u+x $HOME/bin/psxsdk-make
